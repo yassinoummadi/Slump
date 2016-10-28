0dcfefc1aa7b8c5e11f0014feff9c12ce7432261
Yassin Oummadi <yassinoi@vgy.se>
2016-10-28 15:15:31 +0200

    slump

--- Slump/Program.cs
+++ Slump/Program.cs
@@ -10,6 +10,47 @@ namespace Slump
     {
         static void Main(string[] args)
         {
+
+            /* Övning 1
+            Random random = new Random();
+            int summa = 0;
+            for (int i = 0; i < 11; i++)
+            {
+
+                int tal = random.Next(20);
+                summa = summa + tal;
+                Console.WriteLine(tal);
+
+            }
+
+            Console.WriteLine("Medelvärdet är: " + summa/10 );
+            */
+
+            Random random = new Random();
+
+            int tal = 0;
+            int[] lada = new int[20];
+            
+            for (int i = 0; i < 20; i++)
+            {
+                tal = random.Next(15, 31);
+                lada[i] = tal;
+                
+            }
+
+            Array.Sort(lada);
+
+            for (int y = 0; y < lada.Length; y++)
+            {
+                Console.WriteLine(lada[y] + " ");
+            }
+
+            Console.WriteLine("Medianen är lika med " + ((lada[9]) + (lada[10]))/2);
+
+
+
+
+
         }
     }
 }
